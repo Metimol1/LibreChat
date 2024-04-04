ad7f8d5326e42b025955820345535787db417581
Metimol <154080171+Metimol1@users.noreply.github.com>
2024-04-04 13:33:56 +0200

    Update index.js

--- api/app/clients/tools/index.js
+++ api/app/clients/tools/index.js
@@ -1,9 +1,7 @@
-const availableTools = require('./manifest.json');
 const ChatTool = require('./structured/ChatTool');
 const SelfReflectionTool = require('./SelfReflection');
 
 module.exports = {
-  availableTools,
   ChatTool,
   SelfReflectionTool
 }
